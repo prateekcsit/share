33b89085c0b18e82ff7f73e70dcc580994433dca
prateekcsit <prateek.csit@gmail.com>
2023-05-04 08:58:40 +0530

    Add files via upload

---
+++ adi codes/mymath/advmath.py
@@ -0,0 +1,5 @@
+import math
+def power(x,y):
+    return x**y
+def logTen(x):
+    return math.log10(x)
---
+++ adi codes/mymath/basicmath.py
@@ -0,0 +1,12 @@
+''' module to get basic math utilities'''
+def add(*x):
+    '''calculate sum of 2 or more nums'''
+    return sum(x)
+def maximum(*x):
+    '''find max of 2 or more nums'''
+    return max(x)
+def minimum(*x):
+    '''find min of 2 or more nums'''
+    return min(x)
+def msg():
+    return 'this is message from my basic math module'
